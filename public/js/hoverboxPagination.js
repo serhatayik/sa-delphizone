56eb43a6a1f9484d1f65ecbf8d520f5623ffa482
Serhat AYIK <44483411+delphizone@users.noreply.github.com>
2020-10-01 00:02:52 +0300

    Update hoverboxPagination.js

--- public/js/hoverboxPagination.js
+++ public/js/hoverboxPagination.js
@@ -1,89 +1,60 @@
-//All the JS code to append markup for the search and pagination links is in this file.
-//Create pagination links depending on the number of students. We want 10 max per page.
-//The first 10 students are shown when the page loads, and each pagination link displays the correct students.
-//Clicking on “1” in the pagination links should should show students 1 to 10, etc...
-//The correct HTML markup gets appended in the correct place (see filter-example.html) when the page loads.
-//Searching is not case sensitive, you can type "Delano" or "delano"
-//When the search button is pressed, the results should show up.
-//Pagination links should update depending on the number of search results. 
+const galleryItems=document.querySelector(".gallery-items").children;
+ const prev=document.querySelector(".prev");
+ const next=document.querySelector(".next");
+ const page=document.querySelector(".page-num");
+ const maxItem=10;
+ let index=1;
+  
+  const pagination=Math.ceil(galleryItems.length/maxItem);
+  console.log(pagination)
 
-//get all the main elements on the page
-var page = document.querySelector(".page");
-var hoverbox = document.querySelector(".hoverbox");
-var eachItem = document.querySelectorAll(".hoverbox-item");
-var hoverboxDetails = document.querySelector(".hoverbox-details");
-var navlink = document.querySelector('.navlink');
+  prev.addEventListener("click",function(){
+    index--;
+    check();
+    showItems();
+  })
+  next.addEventListener("click",function(){
+  	index++;
+  	check();
+    showItems();  
+  })
 
-//Set the pages variable
-var currentPage = 1;
-var numPages = 0;
-var hoverboxsPerPage = 10;
-var index;
+  function check(){
+  	 if(index==pagination){
+  	 	next.classList.add("disabled");
+  	 }
+  	 else{
+  	   next.classList.remove("disabled");	
+  	 }
 
+  	 if(index==1){
+  	 	prev.classList.add("disabled");
+  	 }
+  	 else{
+  	   prev.classList.remove("disabled");	
+  	 }
+  }
 
-//Recreate pagination system in Js
-var paginationFilter = function pageFilter (nbOfEntries) {
-	var pagination = document.createElement('div');
-	var ulList = document.createElement('ul');
-	var liList = document.createElement('li');
-	var pageLink = document.createElement('a');
-
-	pagination.setAttribute("class", "pagination");
-	pageLink.setAttribute("class", "navlink");
-	pageLink.setAttribute("href", "#");
-
-	pagination.appendChild(ulList);
-	ulList.appendChild(liList);
-	liList.appendChild(pageLink);
-
-	return pagination;	
-};
-
-//Finding the number of students
-
-
-//Display only the 10 first items on the page
-function showImages (number) {
-	for (var i = 0; i < hoverboxsPerPage; i++) {
-		index = number * hoverboxsPerPage - hoverboxsPerPage + i;
-		eachItem[index].style.display = "block";
-		console.log(index);
-	} 
-};
+  function showItems() {
+  	 for(let i=0;i<galleryItems.length; i++){
+  	 	galleryItems[i].classList.remove("show");
+  	 	galleryItems[i].classList.add("hide");
 
 
-//Finding the number of pages
-var numberOfPages = function () {
-	var numberOfPages = parseInt(numberOfStudents() / studentsPerPage);
-	if ( numberOfStudents() % studentsPerPage > 0 ){
-		numPages += 1;
-	}
-	return numberOfPages;
-}
-function createPages () {
-	for (var i = 0; i <= numberOfPages(); i++) {
-		page.appendChild(paginationFilter());
-		var navlink = document.getElementsByClassName(".navlink");
-		var linkText = document.createTextNode(i);
-		//navlink.appendChild('linkText');
-		console.log(linkText);
-	}
-};
+  	    if(i>=(index*maxItem)-maxItem && i<index*maxItem){
 
-var hideAll = function () {
-	for (var i = 0; i < numberOfStudents(); i++) {
-		eachItem[i].style.display = "none";
-	}
-};
-function changePage (number) {
-	document.addEventListener( "DOMContentLoaded", hideAll());
-	document.addEventListener( "DOMContentLoaded", showImages(number));
-}
+          galleryItems[i].classList.remove("hide");
+          galleryItems[i].classList.add("show");
+  	    }
+  	    page.innerHTML=index;
+  	 }
 
-changePage(1);
+  	 	
+  }
 
-
-window.onload = addElements();
-
+  window.onload=function(){
+  	showItems();
+  	check();
+  }
 
 
